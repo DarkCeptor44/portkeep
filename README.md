8097c2ee90f64910080aae86a6433ee7df62b236
DarkCeptor44 <16278483+DarkCeptor44@users.noreply.github.com>
2026-07-24 17:17:11 -0300

    feat(cli): add placeholder serve subcommand

---
+++ README.md
@@ -0,0 +1,15 @@
+# PortKeep
+
+PortKeep is a tool to keep track of your local ports.
+
+## MSRV
+
+The minimum supported Rust version is:
+
+| Version | Edition | MSRV |
+| --- | --- | --- |
+| 0.1.0 | 2024 | 1.85.0 |
+
+## License
+
+This project is licensed under the [Mozilla Public License, version 2.0](https://www.mozilla.org/MPL/2.0/). See the [LICENSE](LICENSE) file for details.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#![forbid(unsafe_code)]
+#![warn(clippy::pedantic)]
+
+use clap::{Parser, Subcommand};
+
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = None, propagate_version = true)]
+pub struct App {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    #[command(about = "Serve portkeep")]
+    Serve,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = App::parse();
+
+    match args.command {
+        Command::Serve => todo!(),
+    }
 }
