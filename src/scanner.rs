4c612db78dca3ccd64826a4ce0802929e72dbe23
DarkCeptor44 <16278483+DarkCeptor44@users.noreply.github.com>
2026-07-27 13:14:29 -0300

    chore(license): add missing license header to more files

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 use crate::config::{PortDetailResponse, PortStatus};
 use netstat2::{AddressFamilyFlags, ProtocolFlags, ProtocolSocketInfo, TcpState, get_sockets_info};
 use std::collections::{BTreeMap, BTreeSet, HashMap};
